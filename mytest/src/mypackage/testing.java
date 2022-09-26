6ab095ef8da37618962acc954217771b5c59f1f7
Munafkhan99 <munafkhan1999@gmail.com>
2022-09-26 15:37:43 +0530

    dytdhf

--- mytest/src/mypackage/testing.java
+++ mytest/src/mypackage/testing.java
@@ -5,7 +5,7 @@ public class testing {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello");
-		System.out.println("world");
+		System.out.println("dd");
 
 
 	}
